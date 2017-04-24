aee262b6207b169d64e311ef325af8bb19376247
Wes Shih <wesshih@uw.edu>
2017-04-24 11:07:02 -0700

    adding function headers in integrate

--- hw2/integrate.py
+++ hw2/integrate.py
@@ -1,10 +1,23 @@
 import numpy as np
 
-def euler():
-  print 'yo'
+def euler_stepper(y, f, h):
+	print 'euler_stepper'
+	# y_n+1 = y_n + h*f(y,h)
+	return y + h*f(y,h)
+
+def mid_stepper(y, f, h):
+	print 'mid_stepper'
+	# k1 = h*f(y,h)
+	# k2 = h*f(y+k1/2, h/2)
+
+def rk_stepper(y, f, h):
+	print 'rk_stepper'
+
+def f(y, h):
+	print 'generic derivative f function'
 
 def main():
-  euler();
+	print 'main'
 
 if __name__ == '__main__':
-  main()
+	main()
